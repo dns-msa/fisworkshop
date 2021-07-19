2dbcef4c90981fb9a081262e94af999c15c81019
Young-ook Kim <youngook.kim@gmail.com>
2021-07-19 19:44:32 +0900

    feat(eks): cloudwatch agent helm chart

--- eks/cdk/eks.py
+++ eks/cdk/eks.py
@@ -26,6 +26,23 @@ class EKS(core.Stack):
 
         mng.role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchAgentServerPolicy"))
 
+        eks.add_helm_chart("aws-cloudwatch-metrics",
+            chart = "aws-cloudwatch-metrics",
+            repository = "https://aws.github.io/eks-charts",
+            namespace = "amazon-cloudwatch",
+            values = {'clusterName': eks.cluster_name}
+        )
+
+        eks.add_helm_chart("aws-for-fluent-bit",
+            chart = "aws-for-fluent-bit",
+            repository = "https://aws.github.io/eks-charts",
+            namespace = "kube-system",
+            values = {
+                'cloudWatch.region': core.Stack.of(self).region,
+                'cloudWatch.logGroupName': f"/aws/containerinsights/{eks.cluster_name}/application"
+            }
+        )
+
         self.output_props = props.copy()
         self.output_props['asg_name']= mng.nodegroup_name
         self.output_props['asg_arn']= mng.nodegroup_arn
