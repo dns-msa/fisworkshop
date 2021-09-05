f84b7cacfec0eb7b33a7253a031079af6f622a99
EC2 Default User <ec2-user@ip-172-31-26-25.ec2.internal>
2021-09-05 15:18:05 +0000

    ASG Lifecycle hook 추가

--- ec2/chaos-stack/lib/chaos-product-composite-stack.ts
+++ ec2/chaos-stack/lib/chaos-product-composite-stack.ts
@@ -91,7 +91,7 @@ export class ChaosProductCompositeStack extends cdk.Stack {
         
         # complete signal to asg
         INSTANCE_ID=\`curl http://169.254.169.254/latest/meta-data/instance-id\`
-        ASG_ID=\`aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[? Tags[? (Key=='aws:cloudformation:stack-name') && Value=='ChaosProductCompositeStack']]".AutoScalingGroupName --output text\`
+        ASG_ID=\`aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[? Tags[? (Key=='aws:cloudformation:stack-name') && Value=='ChaosProductCompositeStack']]".AutoScalingGroupName --output text --region ${process.env.CDK_DEFAULT_REGION}\`
         aws autoscaling complete-lifecycle-action --lifecycle-action-result CONTINUE \
           --instance-id $INSTANCE_ID --lifecycle-hook-name productCompositeAsgLc \
           --auto-scaling-group-name $ASG_ID \
